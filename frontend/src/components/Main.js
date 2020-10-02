7689a5fa6191b3d424051613b1a17e00219c6dca
enriquepablo <enrique@cazalla.net>
2020-10-02 14:08:18 +0200

    add footer

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FormattedMessage } from "react-intl";
 
+import FooterContainer from "containers/Footer";
+
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "styles/Main.scss";
 
@@ -9,11 +11,14 @@ import "styles/Main.scss";
 class Main extends Component {
   render() {
     return (
-      <div>
-        <span id="main-content">
-          <FormattedMessage defaultMessage="Main Component" key="test" />
-        </span>
-      </div>
+      <>
+        <section id="panel">
+          <span id="main-content">
+            <FormattedMessage defaultMessage="Main Component" key="test" />
+          </span>
+          <FooterContainer />
+        </section>
+      </>
     );
   }
 }
