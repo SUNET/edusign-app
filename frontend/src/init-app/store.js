fcead2f52f92ea1b9649c5557c27a0bac73f1086
enriquepablo <enrique@cazalla.net>
2020-10-05 15:38:10 +0200

    notifications slice (actions & reducer)

--- frontend/src/init-app/store.js
+++ frontend/src/init-app/store.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 import { intlReducer } from "react-intl-redux";
 
+import notifications from "slices/Notifications";
+
 export default combineReducers({
   intl: intlReducer,
+  notifications,
 });
