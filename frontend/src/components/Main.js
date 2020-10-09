340dc9a585de965609b6beb22acda0bd7ddc4247
enriquepablo <enrique@cazalla.net>
2020-10-09 11:42:26 +0200

    initial PDF form

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { FormattedMessage } from "react-intl";
 
 import Header from "components/Header";
 import SplashContainer from "containers/Splash";
 import FooterContainer from "containers/Footer";
+import DocumentForm from "components/DocumentForm";
 
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "styles/reset.scss";
@@ -18,7 +18,7 @@ class Main extends Component {
         <section id="panel">
           <Header />
           <span id="main-content">
-            <FormattedMessage defaultMessage="Main Component" key="test" />
+            <DocumentForm />
           </span>
           <FooterContainer />
         </section>
