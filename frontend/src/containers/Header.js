b64ec321900202890c1d0e26f2932c3549ef82b6
enriquepablo <enrique@cazalla.net>
2020-11-24 12:57:15 +0100

    some UI adjustments

---
+++ frontend/src/containers/Header.js
@@ -0,0 +1,23 @@
+/**
+ * @module containers/Header
+ * @desc In this module we connect the Header component with the Redux store,
+ * basically to obtain the given name and surname.
+ */
+import { connect } from "react-redux";
+
+import Header from "components/Header";
+
+const mapStateToProps = (state, props) => {
+  if (state.main.config === undefined) {
+    return {
+      loading: true,
+    }
+  }
+  return {
+      loading: false,
+      givenName: state.main.config.payload.given_name,
+      surname: state.main.config.payload.surname,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
