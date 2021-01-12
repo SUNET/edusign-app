84f1c58fe358962d0d07a0260784119e16b0ead5
enriquepablo <enrique@cazalla.net>
2021-01-12 15:18:02 +0100

    more backend tests

--- backend/src/edusign_webapp/test_app.py
+++ backend/src/edusign_webapp/test_app.py
@@ -148,3 +148,53 @@ def test_add_document(client, monkeypatch):
     resp_data = json.loads(response.data)
 
     assert resp_data['payload']['ref'] == 'ba26478f-f8e0-43db-991c-08af7c65ed58'
+
+
+def test_create_sign_request(client, monkeypatch):
+
+    from edusign_webapp.api_client import APIClient
+
+    def mock_post(*args, **kwargs):
+        return {'binding': 'POST/XML/1.0',
+                'destinationUrl': 'https://sig.idsec.se/sigservice-dev/request',
+                'relayState': '31dc573b-ab7d-496c-845e-cae8792ba063',
+                'signRequest': 'DUMMY SIGN REQUEST',
+                'state': {'id': '31dc573b-ab7d-496c-845e-cae8792ba063'}}
+
+    monkeypatch.setattr(APIClient, '_post', mock_post)
+
+    response1 = client.get('/sign/')
+
+    assert response1.status == '200 OK'
+
+    with run.app.test_request_context():
+        with client.session_transaction() as sess:
+
+            csrf_token = ResponseSchema().get_csrf_token({}, sess=sess)['csrf_token']
+            user_key = sess['user_key']
+
+    from flask.sessions import SecureCookieSession
+
+    def mock_getitem(self, key):
+        if key == 'user_key':
+            return user_key
+        self.accessed = True
+        return super(SecureCookieSession, self).__getitem__(key)
+
+    monkeypatch.setattr(SecureCookieSession, '__getitem__', mock_getitem)
+
+    doc_data = {'csrf_token': csrf_token,
+                'payload': {'documents': [
+                    {'name': 'test4.pdf',
+                     'type': 'application/pdf',
+                     'ref': 'd2a05a27-6913-47ed-82f5-fd0e89ee5f07',
+                     'sign_requirement': '{"fieldValues": {"idp": "https://login.idp.eduid.se/idp.xml"}, "page": 2, "scale": -74, "signerName": {"formatting": null, "signerAttributes": [{"name": "urn:oid:2.5.4.42"}, {"name": "urn:oid:2.5.4.4"}, {"name": "urn:oid:0.9.2342.19200300.100.1.3"}]}, "templateImageRef": "eduSign-image", "xposition": 37, "yposition": 165}'}]}}
+
+    response = client.post('/sign/create-sign-request', headers={'X-Requested-With': 'XMLHttpRequest', 'Origin': 'https://test.localhost', 'X-Forwarded-Host': 'test.localhost'}, json=doc_data)
+
+    assert response.status == '200 OK'
+
+    resp_data = json.loads(response.data)
+
+    assert resp_data['payload']['documents'][0]['name'] == 'test4.pdf'
+    assert resp_data['payload']['relay_state'] == '31dc573b-ab7d-496c-845e-cae8792ba063'
