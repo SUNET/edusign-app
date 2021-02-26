1d7ebc516af1de42cdc1aff72359f3b6ed4ad047
enriquepablo <enrique@cazalla.net>
2021-02-26 11:43:16 +0100

    keep values when modal is hidden

--- frontend/src/components/InviteForm.js
+++ frontend/src/components/InviteForm.js
@@ -60,110 +60,113 @@ const InviteForm = (props) => {
         </Button>
       </div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Formik
-          initialValues={initialValues(props.docId)}
-          onSubmit={(values) => {props.handleSubmit(values)}}
-        >
-          {(fprops) => (
-          <Form>
-            <Field type="hidden" name="documentId" value={fprops.values.documentId} />
-            <Modal.Header closeButton>
-              <Modal.Title>
-                <FormattedMessage
-                  defaultMessage="Invite people to sign document"
-                  key="invite-people"
-                />
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <FieldArray name="invitees">
-                {(arrayHelpers) => (
-                  <div>
-                    {fprops.values.invitees.length > 0 &&
-                      fprops.values.invitees.map((invitee, index) => (
-                        <div className="row" key={index}>
+      <Formik
+        initialValues={initialValues(props.docId)}
+        onSubmit={(values) => {
+          handleClose();
+          props.handleSubmit(values);
+        }}
+      >
+        {(fprops) => (
+          <Modal show={show} onHide={handleClose}>
+            <Form>
+              <Field type="hidden" name="documentId" value={fprops.values.documentId} />
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  <FormattedMessage
+                    defaultMessage="Invite people to sign document"
+                    key="invite-people"
+                  />
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <FieldArray name="invitees">
+                  {(arrayHelpers) => (
+                    <div>
+                      {fprops.values.invitees.length > 0 &&
+                        fprops.values.invitees.map((invitee, index) => (
+                          <div className="row" key={index}>
 
-                          <BForm.Group>
-                            <BForm.Label htmlFor={`invitees.${index}.name`}>
-                              <FormattedMessage defaultMessage="Name" key="name-input-field" />
-                            </BForm.Label>
-                            <Field
-                              name={`invitees.${index}.name`}
-                              data-testid={`invitees.${index}.name`}
-                              value={invitee.name}
-                              placeholder="Jane Doe"
-                              as={BForm.Control}
-                              type="text"
-                              validate={validateName}
-                            />
-                            <ErrorMessage
-                              name={`invitees.${index}.name`}
-                              component="div"
-                              className="field-error"
-                            />
-                          <BForm.Group>
-                          </BForm.Group>
-                            <BForm.Label htmlFor={`invitees.${index}.email`}>
-                              <FormattedMessage defaultMessage="Email" key="email-input-field" />
-                            </BForm.Label>
-                            <Field
-                              name={`invitees.${index}.email`}
-                              data-testid={`invitees.${index}.email`}
-                              value={invitee.email}
-                              placeholder="jane@example.com"
-                              as={BForm.Control}
-                              type="email"
-                              validate={validateEmail}
-                            />
-                            <ErrorMessage
-                              name={`invitees.${index}.email`}
-                              component="div"
-                              className="field-error"
-                            />
-                          </BForm.Group>
-                          <div className="col">
-                            <button
-                              type="button"
-                              className="secondary"
-                              onClick={() => arrayHelpers.remove(index)}
-                            >
-                              X
-                            </button>
+                            <BForm.Group>
+                              <BForm.Label htmlFor={`invitees.${index}.name`}>
+                                <FormattedMessage defaultMessage="Name" key="name-input-field" />
+                              </BForm.Label>
+                              <Field
+                                name={`invitees.${index}.name`}
+                                data-testid={`invitees.${index}.name`}
+                                value={invitee.name}
+                                placeholder="Jane Doe"
+                                as={BForm.Control}
+                                type="text"
+                                validate={validateName}
+                              />
+                              <ErrorMessage
+                                name={`invitees.${index}.name`}
+                                component="div"
+                                className="field-error"
+                              />
+                            <BForm.Group>
+                            </BForm.Group>
+                              <BForm.Label htmlFor={`invitees.${index}.email`}>
+                                <FormattedMessage defaultMessage="Email" key="email-input-field" />
+                              </BForm.Label>
+                              <Field
+                                name={`invitees.${index}.email`}
+                                data-testid={`invitees.${index}.email`}
+                                value={invitee.email}
+                                placeholder="jane@example.com"
+                                as={BForm.Control}
+                                type="email"
+                                validate={validateEmail}
+                              />
+                              <ErrorMessage
+                                name={`invitees.${index}.email`}
+                                component="div"
+                                className="field-error"
+                              />
+                            </BForm.Group>
+                            <div className="col">
+                              <button
+                                type="button"
+                                className="secondary"
+                                onClick={() => arrayHelpers.remove(index)}
+                              >
+                                X
+                              </button>
+                            </div>
                           </div>
-                        </div>
-                      ))}
-                    <Button
-                      variant="secondary"
-                      onClick={() => arrayHelpers.push({ name: '', email: '' })}
-                    >
-                      <FormattedMessage
-                        defaultMessage="Add Invitation"
-                        key="add-invite"
-                      />
-                    </Button>
-                  </div>
-                )}
-              </FieldArray>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                <FormattedMessage
-                  defaultMessage="Cancel"
-                  key="cancel-invite"
-                />
-              </Button>
-              <Button variant="primary" type="submit">
-                <FormattedMessage
-                  defaultMessage="Send"
-                  key="send-invite"
-                />
-              </Button>
-            </Modal.Footer>
-          </Form>
-          )}
-        </Formik>
-      </Modal>
+                        ))}
+                      <Button
+                        variant="secondary"
+                        onClick={() => arrayHelpers.push({ name: '', email: '' })}
+                      >
+                        <FormattedMessage
+                          defaultMessage="Add Invitation"
+                          key="add-invite"
+                        />
+                      </Button>
+                    </div>
+                  )}
+                </FieldArray>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  <FormattedMessage
+                    defaultMessage="Cancel"
+                    key="cancel-invite"
+                  />
+                </Button>
+                <Button variant="primary" type="submit">
+                  <FormattedMessage
+                    defaultMessage="Send"
+                    key="send-invite"
+                  />
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal>
+        )}
+      </Formik>
     </>
   )
 };
