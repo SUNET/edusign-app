6aaddedb83ee19b5945a8f36ede8d93faed08d3d
enriquepablo <enrique@cazalla.net>
2021-09-30 13:07:17 +0200

    fix final signature

--- frontend/src/slices/Documents.js
+++ frontend/src/slices/Documents.js
@@ -469,7 +469,7 @@ const fetchSignedDocuments = async (thunkAPI, dataElem, intl) => {
       thunkAPI.dispatch(
         documentsSlice.actions.updateDocumentWithSignedContent(doc)
       );
-      thunkAPI.dispatch(removeInvites({ doc: doc, intl: intl }));
+        thunkAPI.dispatch(removeOwned({key: doc.id}));
     });
   } catch (err) {
     thunkAPI.dispatch(
