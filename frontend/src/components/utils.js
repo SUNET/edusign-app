4c1b61e33c5fbaf7df6e9952563abf261bf5ada6
enriquepablo <enrique@cazalla.net>
2021-10-01 13:28:03 +0200

    one database per user in the same browser instance

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -109,3 +109,20 @@ export function docToFile(doc) {
     return null;
   }
 }
+
+/**
+ * @public
+ * @function hashCode
+ * @desc Hash string
+ *
+ */
+export const hashCode = function(s) {
+  var hash = 0, i, chr;
+  if (s.length === 0) return hash;
+  for (i = 0; i < s.length; i++) {
+    chr   = s.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash.toString();
+};
