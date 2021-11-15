85118e51e55a9bee202674d31100a578e471ad32
enriquepablo <enrique@cazalla.net>
2021-11-15 14:32:12 +0100

    translations

--- backend/src/edusign_webapp/views.py
+++ backend/src/edusign_webapp/views.py
@@ -657,7 +657,7 @@ def send_multisign_reminder(data: dict) -> dict:
 
     if not pending:
         current_app.logger.error(f"Could not find users pending signing the multi sign request {data['key']}")
-        return {'error': True, 'message': gettext('Problem finding the users pending to multi sign')}
+        return {'error': True, 'message': gettext('Problem finding the users pending to sign')}
 
     if not docname:
         current_app.logger.error(f"Could not find document {data['key']} pending signing the multi sign request")
