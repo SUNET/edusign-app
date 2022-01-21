7f54bc0411837b96cd7d1f5b5f9c857d2b16da61
enriquepablo <enrique@cazalla.net>
2022-01-21 11:16:42 +0100

    sending final email

--- backend/src/edusign_webapp/views.py
+++ backend/src/edusign_webapp/views.py
@@ -628,6 +628,7 @@ def get_signed_documents(sign_data: dict) -> dict:
 
         if 'email' in owner and owner['email'] != session['mail']:
             pending = current_app.doc_store.get_pending_invites(key, exclude=session['mail'])
+            pending = [p for p in pending if not p['signed']]
 
             if len(pending) > 0:
                 template = 'signed_by_email'
