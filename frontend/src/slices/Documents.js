1b306e0ce0ef263bb73c013469b111040e246b12
enriquepablo <enrique@cazalla.net>
2022-11-24 14:18:08 +0100

    signing invitations finally working

--- frontend/src/slices/Documents.js
+++ frontend/src/slices/Documents.js
@@ -794,6 +794,7 @@ export const startSigningDocuments = createAsyncThunk(
       thunkAPI.dispatch(updateSigningForm(formData));
       // Catch errors and inform the user, and update the state with that information.
     } catch (err) {
+      console.log('Error starting signing', err);
       thunkAPI.dispatch(
         addNotification({
           level: "danger",
@@ -963,6 +964,7 @@ export const restartSigningDocuments = createAsyncThunk(
         await thunkAPI.dispatch(checkStoredDocuments());
       }
     } catch (err) {
+      console.log('Error restarting signing', err);
       thunkAPI.dispatch(
         addNotification({
           level: "danger",
@@ -1071,6 +1073,7 @@ const fetchSignedDocuments = async (thunkAPI, dataElem, intl) => {
     });
     await thunkAPI.dispatch(checkStoredDocuments());
   } catch (err) {
+      console.log('Error fetching signed docs', err);
     // In case of errors, notify the user, and update the state.
     thunkAPI.dispatch(
       addNotification({
