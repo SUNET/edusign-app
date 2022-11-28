706edec39b3ed69160164b797a56725d0c987ff4
enriquepablo <enrique@cazalla.net>
2022-11-28 12:48:00 +0100

    fix state after a failure creating a sign request

--- frontend/src/slices/Documents.js
+++ frontend/src/slices/Documents.js
@@ -781,7 +781,6 @@ export const startSigningDocuments = createAsyncThunk(
       thunkAPI.dispatch(updateSigningForm(formData));
       // Catch errors and inform the user, and update the state with that information.
     } catch (err) {
-      console.log('Error starting signing', err);
       thunkAPI.dispatch(
         addNotification({
           level: "danger",
@@ -951,7 +950,6 @@ export const restartSigningDocuments = createAsyncThunk(
         await thunkAPI.dispatch(checkStoredDocuments());
       }
     } catch (err) {
-      console.log('Error restarting signing', err);
       thunkAPI.dispatch(
         addNotification({
           level: "danger",
@@ -1060,7 +1058,6 @@ const fetchSignedDocuments = async (thunkAPI, dataElem, intl) => {
     });
     await thunkAPI.dispatch(checkStoredDocuments());
   } catch (err) {
-      console.log('Error fetching signed docs', err);
     // In case of errors, notify the user, and update the state.
     thunkAPI.dispatch(
       addNotification({
