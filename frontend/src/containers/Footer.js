78722cf3087436ee1742c39bdfffaa2985eeb73e
enriquepablo <enrique@cazalla.net>
2022-12-06 15:43:22 +0100

    site working in spanish

--- frontend/src/containers/Footer.js
+++ frontend/src/containers/Footer.js
@@ -25,7 +25,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     changeLanguage: function (e) {
-      const lang = e.target.closest(".lang-selected").dataset.lang;
+      const lang = e.target.value;
       Cookies.remove("lang");
       Cookies.set("lang", lang, {
         expires: 365,
