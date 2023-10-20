107762e630ea4a250c4054e3790f24d81bf32d53
enriquepablo <enrique@cazalla.net>
2023-10-20 13:52:22 +0200

    correctly extract registration authority

--- backend/src/edusign_webapp/utils.py
+++ backend/src/edusign_webapp/utils.py
@@ -244,9 +244,6 @@ def get_invitations(remove_finished=False):
         else:
             newowned.append(doc)
 
-        if doc['loa'] not in ("", "none"):
-            doc['loa'] += ',' + current_app.config['AVAILABLE_LOAS'][doc['loa']]
-
     return {
         'owned_multisign': newowned,
         'pending_multisign': invited,
