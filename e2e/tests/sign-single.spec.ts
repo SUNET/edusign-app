f2257ae3c6ce1fb05dffb05defbeb7302bd9c5bc
enriquepablo <enrique@cazalla.net>
2024-05-03 19:02:26 +0200

    testing uploads, wip

--- e2e/tests/sign-single.spec.ts
+++ e2e/tests/sign-single.spec.ts
@@ -8,18 +8,17 @@ test('Initial UI with no documents', async ({ browser }) => {
   await page.goto('/sign');
 
   await page.getByTestId('edusign-dnd-area').click();
+  await page.getByTestId('edusign-dnd-area').setInputFiles('./fixtures/test.pdf');
   await expect(page.locator('legend')).toContainText('Personal documents');
-  await page.getByTestId('button-forced-preview-ba66ef11-6c63-4513-88f9-3d8fa7703d14').click();
+  await page.getByTestId('button-forced-preview-test.pdf').click();
   await page.getByTestId('preview-button-confirm-0').click();
   await page.getByTestId('button-sign').click();
   await page.getByPlaceholder('enter password').fill(process.env.USER1_PASS);
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.getByRole('button', { name: 'Use my security key' }).click();
-  await page.goto('https://signservice.test.edusign.sunet.se/sign/idsectest/saml/sso');
-  await page.goto('https://dev.edusign.sunet.se/sign/callback');
-  await expect(page.locator('[id="local-doc-test3\\.pdf"]')).toContainText('Signed by:');
-  await expect(page.locator('[id="local-doc-test3\\.pdf"]')).toContainText('ENRIQUE PABLO PEREZ ARNAUD <enrique@cazalla.net>.');
-  await expect(page.getByTestId('button-download-signed-ba66ef11-6c63-4513-88f9-3d8fa7703d14')).toBeVisible();
+  await expect(page.locator('[id="local-doc-test\\.pdf"]')).toContainText('Signed by:');
+  await expect(page.locator('[id="local-doc-test\\.pdf"]')).toContainText('ENRIQUE PABLO PEREZ ARNAUD <enrique@cazalla.net>.');
+  await expect(page.getByTestId('button-download-signed-test.pdf')).toBeVisible();
 
   await user1Context.close();
 });
