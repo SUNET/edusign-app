7773d41193993232e67993cdce745da82bbe2108
enriquepablo <enrique@cazalla.net>
2024-05-13 10:45:33 +0200

    more e2e tests

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -10,7 +10,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 120000,
+  timeout: 240000,
+  expect: {timeout: 60000},
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
