507388c1a2e47160152ed2717f3472e838b453e1
enriquepablo <enrique@cazalla.net>
2024-05-20 16:32:44 +0200

    working on the e2e tests

--- e2e/tests/load-encrypted-pdf.spec.ts
+++ e2e/tests/load-encrypted-pdf.spec.ts
@@ -1,22 +1,16 @@
 
 import * as path from 'path';
 import { test, expect } from '@playwright/test';
+import { login, addFile } from './utils.ts';
 
 test('Sign one test PDF document', async ({ browser }) => {
-  const user1Context = await browser.newContext({ storageState: 'playwright/.auth/user1.json' });
-  const page = await user1Context.newPage();
 
-  const withKey = process.env.USER1_SECURITY_KEY === "True";
+  const { user0 } = await login(browser, 1);
 
-  await page.goto('/sign');
+  await user0.page.goto('/sign');
 
-  const fileChooserPromise = page.waitForEvent('filechooser');
-  await page.getByTestId('edusign-dnd-area').click();
-  const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles(path.join(__dirname, 'fixtures/encrypted.pdf'));
+  await addFile(user0.page, 'encrypted.pdf');
 
-  await expect(page.locator('[id="local-doc-encrypted\\.pdf"]')).toContainText('Failed to insert sign page');
-
-  await user1Context.close();
+  await expect(user0.page.locator('[id="local-doc-encrypted\\.pdf"]')).toContainText('Failed to insert sign page');
 });
 
