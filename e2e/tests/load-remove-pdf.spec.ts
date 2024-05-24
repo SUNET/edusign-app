9d077a158315dfcf411564eb4816024bf9f327d1
enriquepablo <enrique@cazalla.net>
2024-05-24 18:07:24 +0200

    working on the e2e tests

---
+++ e2e/tests/load-remove-pdf.spec.ts
@@ -0,0 +1,20 @@
+
+import * as path from 'path';
+import { test, expect } from '@playwright/test';
+import { login, addFile, startAtSignPage } from './utils.ts';
+
+test('Load signed PDF document', async ({ browser }) => {
+
+  const filename = 'test.pdf';
+  const { user0 } = await login(browser, 1);
+
+  await startAtSignPage(user0.page);
+
+  await addFile(user0.page, filename);
+
+  await user0.page.getByTestId(`button-forced-preview-${filename}`).click();
+  await user0.page.getByTestId('preview-button-confirm-0').click();
+  await user0.page.getByTestId(`button-rm-invitation-${filename}`).click();
+  await user0.page.getByTestId(`confirm-remove-document-${filename}-confirm-button`).click();
+});
+
