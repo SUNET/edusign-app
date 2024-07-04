e625bef7f1d395f8a96368b9c7c13d4c6d11fc3d
enriquepablo <enrique@cazalla.net>
2024-07-04 18:12:03 +0200

    working on the e2e tests

---
+++ e2e/tests/invite-one-defaults-decline-skip.spec.ts
@@ -0,0 +1,32 @@
+
+import * as path from 'path';
+import { test, expect } from '@playwright/test';
+import { login, addFile, approveForcedPreview, startAtSignPage, makeInvitation, signInvitation, declineInvitation, encodeMailHeader, rmDocument } from './utils.ts';
+import { checkEmails } from './utils-emails.ts';
+
+test('Make one invitation with skip final and decline it', async ({ browser }) => {
+
+  const { user0, user1 } = await login(browser, 2);
+  const filename = 'test.pdf';
+  const signedFilename = 'test-signed.pdf';
+  const draftFilename = 'test-draft.pdf';
+
+  await makeInvitation(user0, [user1], filename, {sendSigned: true, skipFinal: true, ordered: false, loa: 'low'});
+
+  const spec1 = ['invitation', user0, [user1], filename];
+  await checkEmails(user0.page, [spec1]);
+
+  await expect(user0.page.getByRole('group')).toContainText(`Waiting for signatures by:${user1.name} <${user1.email}> .`)
+
+  await declineInvitation(user1, user0, filename, draftFilename)
+
+  const spec2 = ['declined', user0, [user1], filename, {last: true, skip: true}];
+  await checkEmails(user1.page, [spec2]);
+
+  await expect(user0.page.getByRole('group')).toContainText(`Declined by:${user1.name} <${user1.email}> .`);
+
+  await rmDocument(user0, filename, 'invitation');
+
+  const spec3 = ['final-attached', user0, [], filename, {signedFilename: signedFilename}];
+  await checkEmails(user0.page, [spec3]);
+});
