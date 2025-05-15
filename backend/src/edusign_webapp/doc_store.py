44a9cb51e54816d8b78bdb298883624e8de7dae0
enriquepablo <enrique@cazalla.net>
2025-05-15 14:25:13 +0200

    fixing migration to postgres and s3

--- backend/src/edusign_webapp/doc_store.py
+++ backend/src/edusign_webapp/doc_store.py
@@ -532,11 +532,6 @@ class DocStore(object):
 
         self.metadata = docmd_class(app)
 
-    @classmethod
-    def custom(cls, app, old_storage_class_path, old_docmd_class_path):
-        store = cls(app, old_storage_class_path, old_docmd_class_path)
-        return store
-
     def add_document(
         self,
         document: Dict[str, str],
