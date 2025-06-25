50120674a496f262b00635b06a7d1af9bff87e47
enriquepablo <enrique@cazalla.net>
2025-06-25 10:00:17 +0200

    missing assurance level in config

--- backend/src/edusign_webapp/views.py
+++ backend/src/edusign_webapp/views.py
@@ -538,6 +538,7 @@ def get_config() -> dict:
     payload['multisign_buttons'] = current_app.config['MULTISIGN_BUTTONS']
     payload['max_signatures'] = current_app.config['MAX_SIGNATURES']
     payload['available_loas'] = [
+        {'name': gettext('None'), 'value': 'none'},
         {'name': gettext('Low'), 'value': 'low'},
         {'name': gettext('Medium'), 'value': 'medium'},
         {'name': gettext('High'), 'value': 'high'},
