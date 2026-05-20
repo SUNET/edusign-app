7a0afd785df4906c9f1850f69be692613ed44464
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-05-20 15:52:46 +0200

    wrong import

--- backend/src/edusign_webapp/views.py
+++ backend/src/edusign_webapp/views.py
@@ -70,7 +70,6 @@ from edusign_webapp.schemata import (
     ToRestartSigningSchema,
     ToSignSchema,
     SigGlobalSchema,
-    get_authn_context,
 )
 from edusign_webapp.utils import (
     MissingDisplayName,
@@ -88,6 +87,7 @@ from edusign_webapp.utils import (
     pretty_print_xml,
     sendmail,
     sendmail_bulk,
+    get_authn_context,
 )
 
 admin_edusign_views = Blueprint('edusign_admin', __name__, url_prefix='/admin', template_folder='templates')
