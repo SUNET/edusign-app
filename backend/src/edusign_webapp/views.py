b42c44391b6e546db19b21738c102974f7cc006d
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-07-15 09:47:49 +0200

    prevent key error

--- backend/src/edusign_webapp/views.py
+++ backend/src/edusign_webapp/views.py
@@ -971,7 +971,7 @@ def create_sign_request(documents: dict) -> dict:
     except KeyError:
         current_app.logger.error(f'Problem creating sign request, got response: {create_data}')
         # XXX translate
-        return {'error': True, 'message': create_data['message']}
+        return {'error': True, 'message': create_data.get('message', "Error creating sign resquest")}
 
     return {'payload': sign_data}
 
