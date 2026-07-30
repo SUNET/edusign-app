b27897cee38670b56774597add4461b9122b2344
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-07-30 17:39:11 +0200

    wrong value in session

--- backend/src/edusign_webapp/utils.py
+++ backend/src/edusign_webapp/utils.py
@@ -307,7 +307,6 @@ def get_invitations(remove_finished=False, invite_key=''):
             session['mail'] = invite['user']['email']
             session['mail_aliases'] = [invite['user']['email']]
             mail_addresses = session.get('mail_aliases')
-            session['displayName'] = invite['user']['name']
     else:
         owned = current_app.extensions['doc_store'].get_owned_documents(session['eppn'], mail_addresses)
         invited = current_app.extensions['doc_store'].get_pending_documents(mail_addresses)
