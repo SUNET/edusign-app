a02bddcffb8f48a8e83de9fc44ea5eaf8f1dfeff
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-08-06 12:42:35 +0200

    only spin for handlers taht return a promise

--- frontend/src/containers/DropdownItem.js
+++ frontend/src/containers/DropdownItem.js
@@ -10,11 +10,20 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch, props) => {
   return {
     doHandleClick: async function () {
-      if (this.props.disabling) {
-        dispatch(setSpinning(this.props.parentid));
-      }
       const promise = this.props.onClick();
+      // Only spin for handlers that return a promise: a synchronous
+      // handler (e.g. opening a modal) offers no later moment to
+      // unset the spinner.
       if (promise !== undefined) {
+        if (this.props.disabling) {
+          // The id of the dropdown button this item belongs to,
+          // as composed in components/Dropdown.js.
+          dispatch(
+            setSpinning(
+              "dropdown-" + (this.props.doc.key || this.props.doc.name),
+            ),
+          );
+        }
         promise.finally((e) => dispatch(unsetSpinning()));
       }
     },
