a0b68403df794bfe3a725afc9eb310757b1836bd
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-08-19 10:31:10 +0200

    set log level info

--- backend/src/edusign_webapp/run.py
+++ backend/src/edusign_webapp/run.py
@@ -32,6 +32,7 @@
 import pprint
 from importlib import import_module
 from typing import Callable, Optional
+import logging
 
 from flask import Flask, current_app, request
 from flask_babel import Babel
@@ -124,6 +125,8 @@ def edusign_init_app(name: str, config: Optional[dict] = None) -> EduSignApp:
         app.teardown_appcontext(func)
 
     from edusign_webapp import to_translate
+
+    app.logger.setLevel(logging.INFO)
 
     app.logger.info(f'Init {name} app...')
 
